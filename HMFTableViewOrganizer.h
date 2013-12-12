934ff3c3a555d8edd5bd79bb96c5b01dec9b7d7f
Brandon Butler <hackmodford@gmail.com>
2013-12-12 12:04:28 -0500

    Update HMFTableViewOrganizer.h
    Added originalArray as a readonly property.
    Added 2 additional methods to sort without generating sections.

--- HMFTableViewOrganizer.h
+++ HMFTableViewOrganizer.h
@@ -12,9 +12,15 @@
 
 +(instancetype)organizerWithArray:(NSArray *)array;
 
+//a reference to the original array
+@property (nonatomic, strong, readonly) NSArray *originalArray;
+
 -(void)organizeAlphabeticallyWithSortDescriptor:(NSSortDescriptor *)sortDescriptor withInitialSortDescriptor:(NSSortDescriptor *)initialSortDescriptor;
 
 -(void)organizeChronologicallyWithSortDescriptor:(NSSortDescriptor *)sortDescriptor withInitialSortDescriptor:(NSSortDescriptor *)initialSortDescriptor;
+
+-(void)organizeAlphabeticallyWithSortDescriptor:(NSSortDescriptor *)sortDescriptor withSections:(BOOL)withSections;
+-(void)organizeChronologicalyWithSortDescriptor:(NSSortDescriptor *)sortDescriptor withSections:(BOOL)withSections;
 
 -(int)sectionCount;
 -(int)rowCountForSection:(int)section;
